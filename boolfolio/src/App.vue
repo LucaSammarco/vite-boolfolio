588a8b8af0cf1e7943e02cd691f6a333536900bc
Luca Sammarco <luca.sammarco7@gmail.com>
2024-08-21 14:24:51 +0200

    created project and linked to the api

---
+++ boolfolio/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+
+
+import MainHeader from './components/MainHeader.vue';
+import MainApp from './components/MainApp.vue';
+
+import MainFooter from './components/MainFooter.vue';
+
+export default {
+ name : '',
+ components: {
+   MainHeader,
+   MainApp,
+    MainFooter,
+    
+  },
+};
+
+
+</script>
+
+<template>
+  <MainHeader />
+
+  <MainApp />
+
+
+  <MainFooter />
+
+</template>
+
+<style lang="scss">
+
+
+
+</style>
---
+++ boolfolio/src/components/MainApp.vue
@@ -0,0 +1,54 @@
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            projects: [],
+        
+
+        };
+    },
+    methods: {
+        getProjects : function() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+
+           .then((response) => {
+            // handle success
+            console.log(response.data.results.data);
+            this.projects = response.data.results.data;
+            })
+
+            .catch(function (error) {
+              // handle error
+            console.log(error);
+                })
+        }
+
+    },
+    created() {
+        this.getProjects();
+    }
+
+};
+</script>
+
+<template>
+    <div>
+        <h1>Main</h1>
+    </div>
+
+    <ul>
+        <li v-for="project in projects">
+
+            {{ project.name }}
+
+        </li>
+
+    </ul>
+    
+</template>
+
+<style lang="scss" scoped>
+
+</style>
